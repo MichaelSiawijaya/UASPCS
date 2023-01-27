1fac3562af56b8688cd9025d130af9b4a988aa15
MissTy20 <102990056+MissTy20@users.noreply.github.com>
2023-01-27 11:10:47 +0700

    Initial commit

---
+++ app/src/main/java/com/aplikasi/apptokosi01/CallbackInterface.kt
@@ -0,0 +1,6 @@
+package com.aplikasi.apptokosi01
+import com.aplikasi.apptokosi01.response.cart.Cart
+
+interface CallbackInterface {
+    fun passResultCallback(total: String,cart:ArrayList<Cart>)
+}
---
+++ app/src/main/java/com/aplikasi/apptokosi01/response/produk/ProdukResponsePost.kt
@@ -0,0 +1,13 @@
+package com.aplikasi.apptokosi01.response.produk
+
+data class ProdukResponsePost (
+        val `data`: DataProduk,
+        val message: String,
+        val success: Boolean
+        )
+
+data class DataProduk (
+        val `produk`: Produk,
+        val message: String,
+        val success: Boolean
+)
